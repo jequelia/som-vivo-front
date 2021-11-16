7b9ee5d9a8647955b0b998812d73898f590c8267
Gomes, Jequelia Santana <jequelia.s.gomes@accenture.com>
2021-11-16 11:05:23 -0300

    criando estrutura das pastas

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+// import jwt_decode from "jwt-decode";
+// import axios from "axios";
+
+// const url_base = "https://corp-login.k8s-ti-cvc.com.br";
+// const app = axios.create({
+//   baseURL: url_base,
+// });
+
+// app.interceptors.request.use(async (config) => {
+//   var token = localStorage.getItem("app_session");
+//   if (token) {
+//     var decoded = jwt_decode<{ exp: number; iat: number }>(token);
+//     var expirationTime = decoded.exp * 1000;
+//     if (Date.now() >= expirationTime) {
+//       console.log("expirou")
+//     }
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default app;
