5432a7c3edde00a8b100142aee6cc2f4551ead63
Gomes, Jequelia Santana <jequelia.s.gomes@accenture.com>
2021-11-18 21:18:18 -0300

    feature: audio

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/header';
 import { Fab } from '@material-ui/core';
 import {  Flex,MicBox,Container } from "./style";
 import Card from '../../components/card';
 import Mic from '@material-ui/icons/Mic';
-
+ 
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+ 
 
 function Home() {
+
+    const[record, setRecord]= useState('');
+
+      const  onStop = (audioData) => {
+        console.log('audioData', audioData)
+      }
 
     return (
         <Flex>
@@ -17,13 +25,19 @@ function Home() {
                 </Fab>
                 <p>Precione para gravar um áudio</p>
             </MicBox>
+            <div>
+                <div>
+                    <AudioReactRecorder backgroundColor="white"	 state={record} onStop={onStop} />
+                    <button onClick={() => setRecord(RecordState.START)}>Start</button>
+                    <button onClick={() => setRecord(RecordState.STOP)}>Stop</button>
+                </div>
+            </div>
 
             <Container>
                 <Card/>
                 <Card/>
                 <Card/>
                 <Card/>
-
             </Container>
         </Flex>
     );
